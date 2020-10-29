e7e218f7f15408d9f9370a655c8aa077bfee6ce2
Ali Anwar <greenywish@gmail.com>
2020-10-30 03:08:02 +0530

    Create bashcommands.sh

---
+++ UsefulScripts/bashcommands.sh
@@ -0,0 +1,12 @@
+`
+#kernel version
+uname -a
+ls /boot
+cat /proc/version
+
+#distro details and architecture
+hostnamectl
+
+#.viminfo contains copy and pasted strings in registers while using vim
+cat .viminfo
+#can give juicy file and info and strings
